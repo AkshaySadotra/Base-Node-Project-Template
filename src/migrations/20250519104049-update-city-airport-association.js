85e2a49da5d8a98a62fe8d91d31cd630004923dc
AkshaySadotra <akshaysadotra000@gmail.com>
2025-05-19 21:06:59 +0530

    Added associations

---
+++ src/migrations/20250519104049-update-city-airport-association.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.addConstraint('Airports',{
+     type:'FOREIGN KEY',
+     name:'city_fkey_constraint',
+     fields:['cityId'],
+     references:{
+      table:'Cities',
+      field:'id'
+     },
+     onUpdate:'CASCADE',
+     onDelete:'CASCADE',
+   });
+  },
+
+  async down (queryInterface, Sequelize) {
+  // TO remove the constraint or rollback
+  await queryInterface.removeConstraint('Airports', 'city_fkey_constraint');
+  }
+};
