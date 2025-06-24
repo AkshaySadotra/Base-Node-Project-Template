2a97492d2100d63f59e7854e6636e40ae6ffdfb9
AkshaySadotra <akshaysadotra000@gmail.com>
2025-06-24 15:46:22 +0530

    Added new migration

--- src/index.js
+++ src/index.js
@@ -6,8 +6,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use('/api', apiRoutes);
-app.listen(ServerConfig.PORT, ()=>{
+app.listen(ServerConfig.PORT, async ()=>{
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
     // Logger('successfully started the serve ', {});
     // Logger.info('successfully  started the server',{});
+
+    const{Airport, City} = require('./models');
+    
+    // const bombay = await City.findByPk(2);
+    // const bomAirport = await bombay.createAirport({name:'Chhatrapati Shivaji Maharaj International Airport Mumbai', code:'BOM'});
+    // console.log(bomAirport);
+
+    // const bomAirport =await bombay.createAirport({name:'Shirdi International Airport', code:'SAG'});
+    // console.log(bomAirport);
+    const city = await City.findByPk(2);
+
+    await city.destroy({
+        where:{
+            id:2
+        }
+    })
 })
