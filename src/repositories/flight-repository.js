a86d18b5c91102609330221eaad05429df114b46
AkshaySadotra <akshaysadotra000@gmail.com>
2025-06-28 22:44:05 +0530

    Added seat model to our db associated to the airplane

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,5 @@
 const CrudRepository = require('./crud-repository');
-const {Flight, Airplane,Airport} = require('../models')
+const {Flight, Airplane,Airport, City} = require('../models')
 const{Sequelize} = require('sequelize');
 class FlightRepository extends CrudRepository{
     constructor(){
@@ -23,6 +23,10 @@ class FlightRepository extends CrudRepository{
                     as:'departureAirport',
                     on:{
                         col1:Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                    },
+                    include:{
+                        model:City,
+                        required:true
                     }
                 },
                 {
@@ -31,6 +35,10 @@ class FlightRepository extends CrudRepository{
                     as:'arrivalAirport',
                     on:{
                         col1:Sequelize.where(Sequelize.col("Flight.ArrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    },
+                      include:{
+                        model:City,
+                        required:true
                     }
                 }
             ]
---
+++ src/utils/common/emums.js
@@ -0,0 +1,10 @@
+const Seat_type ={
+    BUSINESS:'business',
+    ECONOMY:'economy',
+    PREMIUM_ECONOMY:'premium_ecomomy',
+    FIRST_CLASS:'first-class'
+}
+
+module.exports= {
+    Seat_type
+}
